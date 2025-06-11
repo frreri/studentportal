9df205048ce340dca73cce807076c66394d311df
frreri <49623505+frreri@users.noreply.github.com>
2025-06-11 13:47:08 +0200

    changed to dark mode

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,10 @@ import { NavLink } from "react-router";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav
+      data-bs-theme="dark"
+      className="navbar navbar-expand-lg bg-body-tertiary"
+    >
       <div className="container-fluid">
         <NavLink to="/" className="navbar-brand">
           CodeSchool
