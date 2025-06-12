1b9dacf0b9c69040efdbb082f1eb5b8219f45c74
frreri <49623505+frreri@users.noreply.github.com>
2025-06-12 15:59:54 +0200

    updated navbar to have links on right side

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,7 +20,7 @@ function NavBar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <NavLink to="/" className="nav-link">
                 Home
