21ae3b4f615b57f363b704a294a50447604192a4
frreri <49623505+frreri@users.noreply.github.com>
2025-06-12 14:35:50 +0200

    implemented list of registered courses on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,48 @@
+import { useContext } from "react";
+import { RegistrationContext } from "../contexts/RegistrationContext";
+import { Link } from "react-router";
+
 function Home() {
+  const { registeredCourses } = useContext(RegistrationContext);
   return (
     <div className="container text-center">
       <h1 className="display-1 my-4">Welcome to CodeSchool</h1>
       <p className="lead">
         Start your journey to become a fulltime proffessional developer today
       </p>
+      <div className="container my-5">
+        <h2 className="mb-3">Your Courses</h2>
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-6">
+            <ul className="list-group">
+              {registeredCourses.length > 0 ? (
+                registeredCourses.map((registeredCourse) => (
+                  <li
+                    className="list-group-item d-flex justify-content-between align-items-start py-3"
+                    key={registeredCourse.courseId}
+                  >
+                    <div className="ms-2 me-auto">
+                      <div className="fw-bold">
+                        {registeredCourse.courseName}
+                      </div>
+                    </div>
+                    <span className="badge text-bg-primary rounded-pill">
+                      {registeredCourse.duration}
+                    </span>
+                  </li>
+                ))
+              ) : (
+                <li className="list-group-item py-3">
+                  You have not registered to any courses yet
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+        <Link to="/courses" className="btn btn-primary my-4">
+          To Courses
+        </Link>
+      </div>
     </div>
   );
 }
